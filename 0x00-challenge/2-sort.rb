dfa866a393b24b611b5a1e22d65b9273fc796bdb
JosephAbang <abangjosef@gmail.com>
2023-10-29 09:18:55 +0100

    sort integer arguments in ascending order

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -6,28 +6,23 @@
 
 result = []
 ARGV.each do |arg|
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
     # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
+    next unless arg.match?(/^\d+$/)
 
     # convert to integer
     i_arg = arg.to_i
     
     # insert result at the right position
     is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] <= i_arg
-            i += 1
+    result.each_with_index do |val, i|
+      if i_arg < val
         else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
-        end
+          result.insert(i, i_arg)
+          is_inserted = true
+          break
+      end
     end
-    result << i_arg if !is_inserted
+    result << i_arg unless is_inserted
 end
 
 puts result
